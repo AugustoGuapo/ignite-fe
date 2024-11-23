4e133c0a71e6faeed52c679fdbbcfc22040ec3bb
Maryi Rodriguez Brieva <mrodriguezbrieva@correo.unicordoba.edu.co>
2024-11-22 21:30:06 -0500

    boton revisar

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,8 @@
 const VALUE_UNASSIGNED = 0;
 const VALUE_ASSIGNED = 1;
 const VALUE_IN_PROCESS = 2;
-const VALUE_COMPLETE = 3;
+const VALUE_IN_REVIEW = 3;
+const VALUE_COMPLETE = 4;
 
 // Función para obtener el estado de la tarea como texto
 export function obtenerEstadoTarea(status) {
@@ -9,6 +10,7 @@ export function obtenerEstadoTarea(status) {
         case VALUE_UNASSIGNED: return "Sin Asignar";
         case VALUE_ASSIGNED: return "Asignada";
         case VALUE_IN_PROCESS: return "En Proceso";
+        case VALUE_IN_REVIEW: return "En Revision";
         case VALUE_COMPLETE: return "Completada";
         default: return "Estado Desconocido";
     }
